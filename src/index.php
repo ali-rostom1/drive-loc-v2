be5bdba7582261132d0eaefa12336ae81c4165bc
ali-rostom1 <ali.rostom220@gmail.com>
2024-12-30 17:27:37 +0100

    change the psr-4 root dir to src/classes

--- src/index.php
+++ src/index.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/input.css">
-    <link rel="stylesheet" href="assets/css/output.css">
-    <title>Drive & loc</title>
-</head>
-<body class="bg-black">
-    
-</body>
-</html>
+<?php
+    require_once __DIR__ . '/../vendor/autoload.php';
+    use App\User;
+    $user = new User();
+    $user->name = 'ali';
+    echo $user->name;
+?>
