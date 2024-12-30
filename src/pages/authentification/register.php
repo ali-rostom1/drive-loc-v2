e4391e39f8369b75d124e841bd568ba964baf12b
ali-rostom1 <ali.rostom220@gmail.com>
2024-12-30 16:21:50 +0100

    add login image

--- src/pages/authentification/register.php
+++ src/pages/authentification/register.php
@@ -23,7 +23,7 @@
         <div class="w-full lg:w-1/2 flex flex-col">
 
             <div class="flex justify-center md:justify-start pt-12 md:pl-12 md:-mb-12">
-                <a href="../../" class="bg-red-500 text-white font-bold text-xl p-4">Dine With Us</a>
+                <a href="../../" class="bg-blue-300 text-white font-bold text-xl p-4">Dine With Us</a>
             </div>
 
             <div class="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
@@ -49,10 +49,10 @@
                         <input type="password" id="confirmPassword" placeholder="Password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
                     </div>
     
-                    <input type="submit" value="Register" class="bg-red-500 text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8" />
+                    <input type="submit" value="Register" class="bg-blue-300 text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8" />
                 </form>
                 <div class="text-center pt-12 pb-12">
-                    <p>Already have an account? <a href="login.php" class="underline font-semibold text-red-400">Log in here.</a></p>
+                    <p>Already have an account? <a href="login.php" class="underline font-semibold text-blue-200">Log in here.</a></p>
                 </div>
             </div>
 
@@ -60,7 +60,7 @@
 
         <!-- Image Section -->
         <div class="w-1/2 shadow-2xl">
-            <img class="object-cover w-full h-screen hidden md:block" src="../assets/images/signup_img.jpg" alt="Background" />
+            <img class="object-cover w-full h-screen hidden md:block" src="../../assets/images/signup_img.jpg" alt="Background" />
         </div>
     </div>
     <script src="../../assets/js/register.js"></script>
